458320ccc0d87745957f1aef5d637886ed0f9e7e
Rania Zarka <rani0401@edu.easj.dk>
2020-05-10 13:53:45 +0200

    More Classes created

---
+++ MalkiaMVVM/Singleton/AnimalAdoptersCatalogSingleton.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MalkiaMVVM.Singleton
+{
+    class AnimalAdoptersCatalogSingleton: INotifyPropertyChanged
+    {
+
+        static string a_url = " /api/AnimalsAdopters";
+
+        const string serverURL = "http://localhost:50617/";
+
+
+
+
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
